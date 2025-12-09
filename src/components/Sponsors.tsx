bf16d451c88a3de6c63bdb9b01ae46e627d62458
Satyam Soni <satyampsoni@gmail.com>
2025-12-09 13:47:20 +0530

    added sponsor page

--- src/components/Sponsors.tsx
+++ src/components/Sponsors.tsx
@@ -1,4 +1,6 @@
 
+import Link from "next/link";
+
 export default function Sponsors() {
     return (
         <section id="sponsors" className="relative w-full py-24 bg-gradient-to-b from-[#172B4C] to-[#9FBAC3] min-h-[60vh] flex items-center justify-center overflow-hidden">
@@ -17,14 +19,12 @@ export default function Sponsors() {
                         Join us as a sponsor at Kubernetes Community Days New Delhi and play a key role in empowering India’s cloud-native community. Sponsoring KCD New Delhi offers measurable business value—strengthen brand visibility, generate high-quality leads, position your solutions in front of active adopters, and demonstrate your commitment to the open-source ecosystem.
                     </p>
 
-                    <a 
-                        href="https://www.kcddelhi.com/sponsorship.html"
-                        target="_blank"
-                        rel="noopener noreferrer"
+                    <Link 
+                        href="/sponsorship"
                         className="inline-block bg-[#e2e8f0] hover:bg-white text-[#0f172a] font-bold py-4 px-10 rounded-full transition-all duration-300 shadow-lg hover:shadow-xl text-lg md:text-xl transform hover:-translate-y-1"
                     >
                         Check Sponsorship Opportunities
-                    </a>
+                    </Link>
                    
 
                 </div>
