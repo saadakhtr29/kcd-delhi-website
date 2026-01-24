cdb186f7070036d66fdb066f8356060e5c6866c1
saadakhtr29 <saadakhtr51@gmail.com>
2026-01-25 00:12:24 +0530

    feat(ui): added persistent transparent women discount notification banner and we-make-dev banner to the community partners

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,10 +7,12 @@ import Venue from "@/components/Venue";
 import Team from "@/components/Team";
 import Sponsors from "@/components/Sponsors";
 import Footer from "@/components/Footer";
+import NotificationBanner from "@/components/notificationBanner";
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between">
+    <main className="flex min-h-screen flex-col items-center">
+      <NotificationBanner />
       <Navbar />
       <Hero />
       <About />
